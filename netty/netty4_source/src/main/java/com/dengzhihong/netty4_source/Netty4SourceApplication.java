a1e182eea3665a1e6df83614657d28aaeda78142
邓志鸿 <1353976012@qq.com>
2022-02-09 14:50:09 +0800

    rpc

---
+++ netty/netty4_source/src/main/java/com/dengzhihong/netty4_source/Netty4SourceApplication.java
@@ -0,0 +1,12 @@
+package com.dengzhihong.netty4_source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Netty4SourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Netty4SourceApplication.class, args);
+    }
+}
---
+++ netty/netty4_source/src/test/java/com/dengzhihong/netty4_source/Netty4SourceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dengzhihong.netty4_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Netty4SourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
